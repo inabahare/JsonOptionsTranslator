10c4b18c637011de6b4fda965427fe7a46ddf9e0
inaba <software@panduro.guru>
2019-09-04 13:51:01 +0200

    It now works

---
+++ main.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+const { Translate } = require("@google-cloud/translate");
+const english = require("./english.json");
+
+const languageCode = "el" // https://github.com/libyal/libfwnt/wiki/Language-Code-identifiers
+const projectId = "";
+
+const translated = {};
+
+const translator = new Translate({ projectId });
+
+(async function() {
+    console.log("Translating:")
+    for (const [ key, value ] of Object.entries(english)) {
+        console.log(`\t ${key}`)
+        const [ translation ] = await translator.translate(value, languageCode);
+        
+        translated[key] = translation;
+    }
+    console.log("Done");
+    const jsonString = JSON.stringify(translated);
+
+    console.log("This was what was translated");
+    console.log(jsonString);
+
+    fs.writeFile("./translated", jsonString, () => {
+        console.log("Translations written to disk");
+    })
+})()
